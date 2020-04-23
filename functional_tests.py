621014bd25f95b37321bee01c358902b766aac3b
antptv <ant.ptv@gmail.com>
2020-04-23 10:15:02 +0200

    Using unittest library in functional_test.py to render it cleaner

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,26 @@
 from selenium import webdriver
+import unittest
 
+class NewVisitorTest(unittest.TestCase):
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert "Django" in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app.
+        # She goes to check out its homepage
+        self.browser.get("http://localhost:8000")
 
+        # She notices the page title and header mention to-do -lists
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish the test!")
+
+        # She is invited to enter a to-do item straight awway
+
+        # She types "Buy peacock feather scarf" into a text box
+
+if __name__=="__main__":
+    unittest.main(warnings="ignore")
