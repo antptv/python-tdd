30c1c1083eb99fa98c1f16d4a5ba83ecd0cb1bef
antptv <ant.ptv@gmail.com>
2020-05-04 09:47:07 +0200

    Use bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase 
 import time 
 from selenium.common.exceptions import WebDriverException
 
@@ -8,7 +8,7 @@ from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
