5bfa2f9c79288fe6e345070619f95876f2e9a31e
antptv <ant.ptv@gmail.com>
2020-05-17 17:27:10 +0200

    The code does not work but it is just a stash right now

---
+++ accounts/tests/test_views.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from unittest.mock import patch, call
+
+class SendLoginEmailViewTest(TestCase):
+
+    def test_redirects_to_home_page(self):
+        response = self.client.post(
+            '/accounts/send_login_email',
+            data = {
+                'email': 'not_fan_of_edith@example.com'
+            }
+        )
+        self.assertRedirects(response, '/')
+
+    @patch('accounts.views.send_mail')
+    def test_sends_mail_to_adress_from_post(self, mock_send_mail):
+        self.client.post(
+            '/accounts/send_login_email',
+            data={
+                'email': 'not_fan_of_edith@example.com'
+            }
+        )
+
+        self.assertTrue(mock_send_mail.called)
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertEqual(subject, 'Your login link for Superlists')
+        self.assertEqual(from_email, 'noreply@superlists')
+        self.assertEqual(to_list, ['not_fan_of_edith@example.com'])
+
+    def test_adds_success_message(self):
+        response = self.client.post(
+            '/accounts/send_login_email',
+            data={
+                'email': 'edith@example.com'
+            },
+            follow=True
+        )
+        message = list(response.context['messages'])[0]
+        self.assertEqual(
+            message.message,
+            "Check your email, we've sent you a link you can use to log in."
+        )
+        self.assertEqual(message.tags, "success")
+
