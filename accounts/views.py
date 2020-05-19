5749610a58eb32898213274566271c1423f542be
antptv <ant.ptv@gmail.com>
2020-05-19 15:14:31 +0200

    Custom passwordless auth backend + custom user model

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import redirect
 from django.core.mail import send_mail
-from django.contrib import messages
+from django.contrib import auth, messages
+from accounts.models import Token
+from django.core.urlresolvers import reverse
+
 
 def send_login_email(request):
     email = request.POST['email']
-    print(type(send_mail))
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}' 
     send_mail(
         'Your login link for Superlists',
-        'body text tbc',
+        message_body,
         'noreply@superlists',
         [email],
     )
@@ -17,5 +24,8 @@ def send_login_email(request):
     )
     return redirect('/')
 
-
-
+def login(request):
+    user = auth.authenticate(uid=request.GET.get('token'))
+    if user:
+        auth.login(request, user)
+    return redirect('/')
